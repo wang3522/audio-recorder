b109862add8012e9bb61fe95256deedaa3a069f8
Zhigang  Wang <WANGZ20@na.ad.whirlpool.com>
2018-09-10 23:31:00 -0500

    add 4 sec clip duration

--- client/js/app.js
+++ client/js/app.js
@@ -10,12 +10,10 @@ var AudioContext = window.AudioContext || window.webkitAudioContext;
 var audioContext //audio context to help us record
 
 var recordButton = document.getElementById("recordButton");
-var stopButton = document.getElementById("stopButton");
 var uploadButton = document.getElementById("uploadButton");
 
 //add events to those 2 buttons
 recordButton.addEventListener("click", startRecording);
-stopButton.addEventListener("click", stopRecording);
 uploadButton.addEventListener("click", uploadRecording);
 
 function startRecording() {
@@ -33,14 +31,14 @@ function startRecording() {
   */
 
   recordButton.disabled = true;
-  stopButton.disabled = false;
+  recordButton.innerHTML = "Recording";
 
   /*
       We're using the standard promise based getUserMedia() 
       https://developer.mozilla.org/en-US/docs/Web/API/MediaDevices/getUserMedia
   */
 
-  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
+  navigator.mediaDevices.getUserMedia(constraints).then( stream => {
     console.log("getUserMedia() success, stream created, initializing Recorder.js ...");
 
     /*
@@ -66,12 +64,26 @@ function startRecording() {
     //start the recording process
     rec.record()
 
-    console.log("Recording started");
+    console.log("Recording started"); 
 
+    const sleep = time => new Promise(resolve => setTimeout(resolve, time));
+    (async () => {        
+          await sleep(4000);          
+          console.log("after sleep");
+          rec.stop();
+          gumStream.getAudioTracks()[0].stop();
+          recordButton.disabled = false;
+          recordButton.innerHTML = "Record";
+          rec.exportWAV(createDownloadLink);
+    })();
+  }, reason => {
+    console.log("user denied the access");
+    alert('You need to grant access to microphone in order to finish recording')
   }).catch(function(err) {
       //enable the record button if getUserMedia() fails
       recordButton.disabled = false;
-      stopButton.disabled = true;
+      recordButton.innerHTML = "Record";
+
   });
 }
 
@@ -95,22 +107,6 @@ function uploadRecording(){
   
 }
 
-function stopRecording() {
-  console.log("stopButton clicked");
-
-  //disable the stop button, enable the record too allow for new recordings
-  stopButton.disabled = true;
-  recordButton.disabled = false;
-    
-  //tell the recorder to stop the recording
-  rec.stop();
-
-  //stop microphone access
-  gumStream.getAudioTracks()[0].stop();
-
-  //create the wav blob and pass it on to createDownloadLink
-  rec.exportWAV(createDownloadLink);
-}
 
 function createDownloadLink(blob) {
   
@@ -122,13 +118,15 @@ function createDownloadLink(blob) {
   //name of .wav file to use during upload and download (without extendion)
   var filename = new Date().toISOString();
 
+  // unlikely but still want to avoid file name conflict for upload
+  var randomSuffix = Math.random().toString(36).substr(2, 5);
   //add controls to the <audio> element
   au.controls = true;
   au.src = url;
 
   //save to disk link
   link.href = url;
-  link.download = filename+".wav"; //download forces the browser to donwload the file using the  filename
+  link.download = filename+"."+randomSuffix+".wav"; //download forces the browser to donwload the file using the  filename
   link.innerHTML = "download";
 
   //add the new audio element to li
@@ -140,7 +138,6 @@ function createDownloadLink(blob) {
   //add the save to disk link to li
   li.appendChild(link);
   
-
   //add the li element to the ol
   recordingsList.appendChild(li);
 }
