76cd8b1a9b3eaad935df874c326fc01731b8e082
Zhigang Wang <zhigang_wang@whirlpool.com>
2018-09-27 22:40:30 -0500

    use http-auth for authentication as the index.html should not be password protected. Move recorder to recorder.html

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
-var basicAuth = require('express-basic-auth');
+var auth = require('http-auth');
 var bodyParser = require("body-parser");
 var request = require('request');
 const app = express();
-var config = require('../.audio-recorder-conf/config.json')
+var config = require('./config.json')
+
+
+var basic = auth.basic({
+    realm: 'Your credential is required to use the recorder.'
+}, function(username, password, callback) {
+    callback(username == config.username && password == config.passwd);
+});
 
 const accessToken =  config.access_token;
 const apiUrl = "https://9k9kz5aqu0.execute-api.us-east-1.amazonaws.com/dev/presigned-upload";
@@ -17,17 +24,17 @@ var headers = {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(basicAuth({
-    challenge: true,
-    users: { 'yummly': config.passwd }
-}));
-
 app.use(express.static('client'))
-//app.use('/static', express.static(path.join(__dirname, 'client')));
-//app.get('/',function(req,res){
-//  res.sendFile('index.html', { root: __dirname  + '/client'});
-//});
+// Create middleware that can be used to protect routes with basic auth
+var authMiddleware = auth.connect(basic);
 
+// Protected route
+app.get('/recorder', authMiddleware, function(request, response) {
+    response.sendFile('recorder.html', { root: __dirname  + '/client'});
+
+});
+
+//app.use(express.static(__dirname + '/public'));
 
 app.post('/upload',function(req,res){
 	var filename=req.body.filename;
